b131c6cd5a2d2519b3c399bf30de8a4f27a2d391
RobuxTRex <RobuxTRex@Gmail.com>
2022-07-11 19:05:51 +0100

    Build 1043

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -13,41 +13,37 @@ function Home() {
       <Head>
         <title>{GAME_TITLE}</title>
       </Head>
-      <div>
         <div className='grid grid-col-1 text-2xl w-full text-center'>
           <div className='grid grid-col-1 text-2xl w-full text-center'>
-            <span className="text-5xl font-semibold hover:font-bold">{GAME_TITLE}</span>
+            <span className="text-5xl font-semibold hover:font-bold mt-10">{GAME_TITLE}</span>
             <div className='grid grid-col-1 text-2xl w-full text-centre'>
             </div>
           </div>
         </div>
         <span>
-          <div className='mt-10 w-full flex-wrap flex justify-center'>
-            <div>
+            <div className='mt-10 text-center'>
               <span>
-                <Link href='/gamemodes/singeplayer'>
-                  <a className='btn-blue text-2xl'>Singeplayer</a>
+                <Link href='./gamemodes/singleplayer'>
+                  <a className='bg-gray-900 text-white border text-2xl hover:text-3xl focus:border-blue-900 rounded-xl outline-none p-4 border-gray-400 hover:border-blue-400 w-64 duration-400 hover:duration-400'>Singleplayer</a>
                 </Link>
               </span>
             </div>
-            <div>
+            <div className='mt-10 text-center'>
               <span>
                 <Link href='/gamemodes/multiplayer'>
-                  <a className='btn-blue text-2xl'>Multiplayer</a>
+                  <a className='bg-gray-900 text-white border text-2xl hover:text-3xl focus:border-blue-900 rounded-xl outline-none p-4 border-blue-400 hover:border-blue-600 w-64 duration-400 hover:duration-400'>Multiplayer</a>
                 </Link>
               </span>
             </div>
-            <div>
+            <div className='mt-10 text-center'>
               <span>
                 <Link href='/credits'>
                   <a className='btn-blue text-2xl'>Credits</a>
                 </Link>
               </span>
             </div>
-          </div>
         </span>
         <span><DevFooter/></span>
-      </div>
     </React.Fragment>
   );
 }
